d9790d95076cc11c79e62c05f7088ee20109fda0
PCC <alcides.puerta@udea.edu.co>
2025-05-19 10:46:32 -0500

    eliminacion iconos

--- src/components/molecules/Card/index.tsx
+++ src/components/molecules/Card/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Icon from '@/components/atoms/Icon';
+
 import { SubTitle, SubText } from '@/components/atoms/Titles';
 
 const index = () => {
   return (
     <div>
       <div className='flex flex-col items-center justify-center w-full h-screen'>
-        <Icon icon='material-symbols:add-card-outline' />
+        
         <SubTitle />
         <SubText text='lorem' />
       </div>
